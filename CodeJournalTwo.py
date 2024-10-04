fdf5b1ddf641fdf2011cc49e0a65ddc2b1ca8438
Sangbin914 <slee504@ucsc.edu>
2024-10-03 19:24:17 -0700

    Add files via upload

---
+++ CodeJournalTwo.py
@@ -0,0 +1,21 @@
+#Generate numbers
+FloatNum1 = 18.0
+FloatNum2 = 4.5
+IntNum1 = 5
+IntNum2 = 7
+
+#prints the sum of two floating point numbers and data type
+FloatSum = FloatNum1 + FloatNum2
+print(FloatSum)
+print(type(FloatSum))
+
+#prints the difference between two integers and data type
+IntDiff = IntNum1 - IntNum2
+print(IntDiff)
+print(type(IntDiff))
+
+#prints the product of a floating point number and an integer and data type
+product = FloatNum1 * IntNum1
+print(product)
+print(type(product))
+
